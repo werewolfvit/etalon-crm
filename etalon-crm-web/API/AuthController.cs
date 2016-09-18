9bdb4e8feacb9b61f56c4f75437774e6a595b468
werewolfvit <werewolfvit@gmail.com>
2016-09-18 15:24:22 +0300

    auth ready

---
+++ etalon-crm-web/API/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using etalon_crm_web.Models;
+using etalon_crm_web.Models.Common;
+using etalon_crm_web.Models.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace etalon_crm_web.API
+{
+    public class AuthController : ApiController
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public AuthController() : this(new UserManager<User>(new UserStore()), new RoleManager<Role>(new RoleStore()))
+        {
+        }
+
+        public AuthController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<MessageModel> Login([FromBody] LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return MessageBuilder.GetErrorMessage("Ошибка запроса");
+            }
+
+            var user = await _userManager.FindAsync(model.Login, model.Password);
+            if (user == null)
+                return MessageBuilder.GetErrorMessage(@"Неверная пара логин\пароль!");
+
+            try
+            {
+                await SignInAsync(user, true);
+                return MessageBuilder.GetSuccessMessage(null);
+            }
+            catch (Exception ex)
+            {
+                return MessageBuilder.GetErrorMessage(ex.Message);
+            }
+        }
+
+        [Authorize]
+        public MessageModel Logout()
+        {
+            try
+            {
+                AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+                return MessageBuilder.GetSuccessMessage(null);
+            }
+            catch (Exception ex)
+            {
+                return MessageBuilder.GetErrorMessage(ex.Message);
+            }
+            
+        }
+
+        private async Task SignInAsync(User user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+            var identity = await _userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+            AuthenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, identity);
+        }
+
+
+        private static IAuthenticationManager AuthenticationManager => HttpContext.Current.GetOwinContext().Authentication;
+    }
+}
---
+++ etalon-crm-web/Models/Common/MessageModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace etalon_crm_web.Models.Common
+{
+    public class MessageModel
+    {
+        public string Status;
+        public string Message;
+        public Object Data;
+    }
+
+    public static class MessageBuilder
+    {
+        public static MessageModel GetErrorMessage(string error)
+        {
+            var message = new MessageModel()
+            {
+                Status = "Error",
+                Message = error,
+                Data = string.Empty
+            };
+
+            return message;
+        }
+
+        public static MessageModel GetSuccessMessage(Object data)
+        {
+            var message = new MessageModel()
+            {
+                Status = "OK",
+                Message = string.Empty,
+                Data = data,
+            };
+
+            return message;
+        }
+    }
+}
